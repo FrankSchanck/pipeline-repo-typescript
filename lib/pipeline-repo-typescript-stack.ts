cfaa23af9ab629cb8c77af3d01a8ead4d7ce6fb4
Frank Schanck <frank.schanck@mhp.com>
2020-10-02 11:37:49 +0200

    add lambda and api gw

--- lib/pipeline-repo-typescript-stack.ts
+++ lib/pipeline-repo-typescript-stack.ts
@@ -3,10 +3,14 @@ import { CdkPipeline, SimpleSynthAction } from '@aws-cdk/pipelines';
 
 import * as codepipeline from '@aws-cdk/aws-codepipeline';
 import * as codepipeline_actions from '@aws-cdk/aws-codepipeline-actions';
+import * as codecommit from '@aws-cdk/aws-codecommit';
 
 export class PipelineRepoTypescriptStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
+
+    const code = codecommit.Repository.fromRepositoryName(this, 'ImportedRepo',
+      'cdk-pipeline-typescript');
 
     const sourceArtifact = new codepipeline.Artifact();
     const cloudAssemblyArtifact = new codepipeline.Artifact();
@@ -15,14 +19,23 @@ export class PipelineRepoTypescriptStack extends Stack {
       pipelineName: 'MyAppPipeline',
       cloudAssemblyArtifact,
 
+      /*
       sourceAction: new codepipeline_actions.GitHubSourceAction({
+
         actionName: 'GitHub',
         output: sourceArtifact,
-        oauthToken: SecretValue.secretsManager('GITHUB_TOKEN_NAME'),
+        oauthToken: SecretValue.secretsManager('Github-Token'),
         trigger: codepipeline_actions.GitHubTrigger.POLL,
         // Replace these with your actual GitHub project info
         owner: 'FrankSchanck',
         repo: 'pipeline-repo-typescript',
+      }),
+      */
+
+      sourceAction: new codepipeline_actions.CodeCommitSourceAction({
+        actionName: 'CodeCommit_Source',
+        repository: code,
+        output: sourceArtifact,
       }),
 
       synthAction: SimpleSynthAction.standardNpmSynth({
